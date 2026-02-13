b2decda8a540909999d3b9015b0485f7135f102f
Anil Kondaveni <Anil.kondaveni@techcarrot.ae>
2026-02-13 06:16:19 +0530

    update

--- ticketing_it/__manifest__.py
+++ ticketing_it/__manifest__.py
@@ -17,7 +17,6 @@
         'security/ir.model.access.csv',
         'data/ir_sequence.xml',
         'data/email_templates.xml',
-        # 'data/automated_actions.xml',
         'views/it_ticket_views.xml',
         'views/portal_templates.xml',
         'views/menu_items.xml',
